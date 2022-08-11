11f04ad6bb537e6ccacf8f23cad02ce360b766f6
Matthew Kennedy <m.kennedy@me.com>
2022-08-11 16:06:39 +0100

    Find

--- lib/spree/backend/testing_support/spree_admin_rake.rb
+++ lib/spree/backend/testing_support/spree_admin_rake.rb
@@ -46,7 +46,8 @@ namespace :spree_admin do
         $stdout.puts "Installing Spree Admin node dependencies..."
 
         if ENV["CI_NAME"] == "CIRCLE_CI"
-          system("cd ../../spec/dummy && yarn add file:../../spree_admin")
+          system("ls")
+          system("cd ../../spec/dummy && yarn add file:./spree_admin")
         else
           system("yarn add file:../../spree_admin")
         end
