b80b602ac513b7812131c0a13728c36ca3cd9084
Matthew Kennedy <m.kennedy@me.com>
2022-08-11 21:35:51 +0100

    Test

--- lib/spree/backend/testing_support/spree_admin_rake.rb
+++ lib/spree/backend/testing_support/spree_admin_rake.rb
@@ -48,8 +48,10 @@ namespace :spree_admin do
         $stdout.puts "Installing Spree Admin node dependencies..."
 
         system("yarn add file:./../../../spree_admin")
+        system('yarn link @spree/admin')
+        system('yarn')
 
-        $stdout.puts "Adding Spree Admin assets after @Spree/admin is installed by yarn..."
+        $stdout.puts "Adding Spree Admin assets after @spree/admin installed by yarn..."
         ENV["RAILS_ENV"] = "development"
 
         system("bin/rails g spree:backend:install")
