6fe6aa05e32c79605ab8f81c1b60aa2e3cfe2479
Matthew Kennedy <m.kennedy@me.com>
2022-08-31 09:20:41 +0100

    Improve copy.

--- app/javascript/spree/admin/controllers/clipboard_controller.js
+++ app/javascript/spree/admin/controllers/clipboard_controller.js
@@ -1,11 +1,16 @@
 import { Controller } from '@hotwired/stimulus'
+import { flashToastNotice } from '../utilities/bs_toast'
 
 export default class extends Controller {
   static targets = ['source']
 
   copy (event) {
-    event.preventDefault()
-    this.sourceTarget.select()
-    document.execCommand('copy')
+    navigator.clipboard.writeText(this.sourceTarget.value).then(() => {
+      console.log('Successfully copied to clipboard')
+      flashToastNotice(`Successfully copied ${this.sourceTarget.placeholder} to clipboard`)
+    }, () => {
+      console.log('Failed to copy clipboard')
+      flashToastNotice(`Failed to copy ${this.sourceTarget.placeholder} to clipboard`)
+    })
   }
 }
