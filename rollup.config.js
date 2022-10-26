8f0acc7b97a24c5550b8507441b3e9748a883cd4
Matthew Kennedy <m.kennedy@me.com>
2022-10-27 00:32:20 +0100

    Improve Rollup config and output. (#42)
    * Improve Rollup config and output.
    
    * Clean up file section of package.json
    
    * v0.0.9
    
    * Clean up rollup.config
    
    * Add missing dev dependencies.
    
    * Remove unneeded js import from manifest.

--- rollup.config.js
+++ rollup.config.js
@@ -29,42 +29,51 @@ const postCssOptions = {
 }
 
 export default [
+
+  // CSS
   {
-    input: 'app/javascript/spree/backend/index.js',
+    input: './postcss_styles.js',
     output: [
-      // Outputs for Rails Gem and live SCSS in development mode with yarn watch
       {
+        name: 'CSS For Rails Gem',
         file: 'app/assets/stylesheets/spree_admin.css',
         inlineDynamicImports: true,
         format: 'es',
         sourcemap: true
       },
-
-      // Outputs for NPM package
       {
+        name: 'CSS For NPM',
         file: 'dist/css/spree_admin.css',
         inlineDynamicImports: true,
         format: 'es',
-        name: 'SpreeAdminCSS',
         sourcemap: true
-      },
+      }
+    ],
+    plugins: [
+      postcss(postCssOptions)
+    ]
+  },
+
+  // JAVASCRIPT
+  {
+    input: 'app/javascript/spree/backend/index.js',
+    output: [
       {
+        name: 'UDM JavaScript For NPM',
         file: 'dist/js/spree_admin.js',
         format: 'umd',
-        name: 'SpreeAdminJs',
         inlineDynamicImports: true,
         sourcemap: true
       },
       {
+        name: 'ESM JavaScript For NPM',
         file: 'dist/js/spree_admin.esm.js',
         format: 'es',
-        name: 'SpreeAdminEsm',
         inlineDynamicImports: true,
         sourcemap: true
       }
     ],
     plugins: [
-      postcss(postCssOptions),
       resolve(),
       commonjs(),
       terser(terserOptions)
