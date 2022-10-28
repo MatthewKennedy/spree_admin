e99aa99bd1f4e0b0ca7fba3fc7eb5f03e23b06c9
Matthew Kennedy <m.kennedy@me.com>
2022-10-28 02:07:43 +0100

    Remove npm testing.

--- lib/generators/spree/backend/install/install_generator.rb
+++ lib/generators/spree/backend/install/install_generator.rb
@@ -13,12 +13,7 @@ module Spree
         end
 
         def install
-          # template "app/assets/config/manifest.js", force: true if Rails.env == "test"
           template "vendor/assets/stylesheets/spree/backend/all.css"
-
-          unless ENV["SPREE_ADMIN_SKIP_INSTALL_NODE_JS_FILES"] == "true"
-            template "app/javascript/spree_admin.js"
-          end
         end
       end
     end
