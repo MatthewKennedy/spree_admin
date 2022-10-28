d2efd85dac2bf7f70b88deb89facc31b834d7b67
Matthew Kennedy <m.kennedy@me.com>
2022-10-28 11:36:36 +0100

    Refine process.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,4 @@
 Rails.application.config.assets.precompile << "spree_admin_manifest.js"
-Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/icons")
-
 Rails.application.config.assets.configure do |env|
   env.export_concurrent = false
 end
